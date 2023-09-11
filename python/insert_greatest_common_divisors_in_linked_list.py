641f86275d6969c3716763a674328023537fb3a7
nguyenly140 <nguyenly140@gmail.com>
2023-09-10 17:52:58 -0700

    insertGreatestCommonDivisiors

---
+++ python/insert_greatest_common_divisors_in_linked_list.py
@@ -0,0 +1,9 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        print(head.val)
+        test = head.next
